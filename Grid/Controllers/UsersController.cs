325b5a29524d1899c40e80e4ee96eafedafb7c6d
Arathy V <2000078310@hexaware.com>
2023-05-31 15:57:41 +0530

    Backend

---
+++ Grid/Controllers/UsersController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Grid.Models;
+
+namespace Grid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public UsersController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return _context.Users;
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser([FromRoute] int id, [FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.Sno)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<IActionResult> PostUser([FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Sno }, user);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+        [HttpPost("loginUser")]
+        public async Task<IActionResult> LoginUser([FromBody] User loginObj)
+        {
+            if (loginObj == null) return BadRequest();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == loginObj.Email);
+            if (user == null) return NotFound(new { Message = "User Not Found" });
+            var pass = await _context.Users.FirstOrDefaultAsync(x => x.Password == loginObj.Password);
+            if (pass == null) return NotFound(new { Message = "Wrong Password" });
+            var login = await _context.Users.FirstOrDefaultAsync(x => x.Email == loginObj.Email && x.Password == loginObj.Password);
+            if (login == null) { return NotFound(new { Message = "User Not Found here" }); }
+            return Ok(new
+            {
+                Message = "Login success"
+            });
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Sno == id);
+        }
+    }
+  
+
+}
